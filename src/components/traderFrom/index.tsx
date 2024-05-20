2f04e674f8ca8317fff96f8f3743d5706c7b3cd1
Muhammad Rizwan Rasheed <rixwan.rizu@gmail.com>
2024-05-20 11:39:13 +0100

    added comments

--- src/components/traderFrom/index.tsx
+++ src/components/traderFrom/index.tsx
@@ -44,30 +44,40 @@ const TradeForm: FC = () => {
 
     if (formValue.instrument === "Option") {
 
+      // The lastPrice should lie between the low and high prices to ensure it is within the recorded daily price range.
       if (lastPrice < low || lastPrice > high) {
         message.push(`Last price (${lastPrice}) is not consistent with low (${low}) and high (${high}) prices.`);
       }
 
+      // Typically, the open price of a trading period (e.g., a day) should match the lastPrice from the previous period. This ensures continuity and accuracy in pricing.
       if (open !== lastPrice) {
         message.push(`Open price (${open}) should be equal to the last price (${lastPrice}).`);
       }
 
+      //  In financial markets, the bidPrice (the highest price a buyer is willing to pay) should always be lower than the 
+      // askPrice (the lowest price a seller is willing to accept). If bidPrice is equal to or higher than askPrice, it indicates a market anomaly or data error.
       if (bidPrice >= askPrice) {
         message.push(`Bid price (${bidPrice}) should be less than ask price (${askPrice}).`);
       }
 
+      // If there are no trades (volume is zero), it logically follows that the total traded amount should also be zero. Any non-zero amount in this case would be incorrect.
       if (volume === 0 && amount !== 0) {
         message.push('If volume is zero, amount should also be zero. ');
       }
 
+      // A non-zero percentage change in price should correspond to a non-zero absolute change in price. 
+      // If priceChange is zero while priceChangePercent is non-zero, it indicates a data inconsistency.
       if (priceChangePercent !== 0 && priceChange === 0) {
         message.push('If price change percent is non-zero, price change should not be zero.');
       }
 
+      // A non-zero trading volume implies that trades have occurred. Therefore, the number of trades (tradeCount) should be non-zero.
+      // If tradeCount is zero, it suggests a discrepancy in the trade data.
       if (volume > 0 && tradeCount === 0) {
         message.push('If volume is non-zero, trade count should also be non-zero.');
       }
 
+      // Options are typically set to expire at a future date. An expiration date in the past or present is not valid for a new trade and indicates an error in the data.
       if (new Date(formValue.expirationDate) <= currentDate) {
         message.push(`Expiration date (${formValue.expirationDate}) is not a valid future date.`);
       }
